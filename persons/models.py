cbfe2062535943c63eb95a6f9704a49f00762974
Bruno-Felix <balvesfelix@gmail.com>
2024-06-28 11:14:44 -0300

    Criando endpoints de Disciplinas cadastradas ao aluno

--- persons/models.py
+++ persons/models.py
@@ -1,8 +1,8 @@
-from django.utils import timezone
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.utils.translation import gettext_lazy as _
-from django.core.exceptions import ObjectDoesNotExist
+
+from subjects.models import Subject
 
 from .managers import PersonManager
 
@@ -19,9 +19,26 @@ class Person(AbstractUser):
         null=False
     )
 
+    subjects = models.ManyToManyField(Subject, blank=True)
+
     username = None
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
 
     def __str__(self) -> str:
         return f'{self.name} [{self.email}]'
+    
+
+    def add_all_subject_studied(self, subject_list):
+        try:
+            self.subjects.clear()
+            
+            for sub_id in subject_list:
+                subject_studied = Subject.objects.get(pk = sub_id)
+
+                self.subjects.add(subject_studied)
+        except Exception as error:
+            raise Exception(str(error))
+        
+    def get_all_subjects(self):
+        return self.subjects.all()
--- persons/views.py
+++ persons/views.py
@@ -1,8 +1,13 @@
 from rest_framework import status
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import AllowAny
+from rest_framework.decorators import action
+from rest_framework.request import Request
+from rest_framework.response import Response
+
 from .models import Person
 from .serializers import PersonSerializer
+from subjects.serializers import SubjectSerializer
 
 
 class PersonViewSet(ModelViewSet):
@@ -13,3 +18,28 @@ class PersonViewSet(ModelViewSet):
         if self.action == 'create':
             return [AllowAny()]
         return super().get_permissions()
+    
+    @action(detail=True, methods=['post'], url_path='add-subject')
+    def add_subjects(self, request: Request, pk=None):
+        user: Person = self.get_object()
+
+        data = request.data
+
+        try:
+            user.add_all_subject_studied(data)
+        except Exception as e:
+            return Response({'errors': e.args}, status=status.HTTP_400_BAD_REQUEST)
+
+        return Response({'message': 'Disciplinas cadastradas ao aluno com sucesso'}, status.HTTP_200_OK)
+    
+    @action(detail=True, methods=['get'], url_path='subject')
+    def subjects(self, request: Request, pk=None):
+        user: Person = self.get_object()
+
+        try:
+            subjects_list = user.get_all_subjects()
+            serializer = SubjectSerializer(subjects_list, many=True, context={'request': request})
+        except Exception as e:
+            return Response({'errors': e.args}, status=status.HTTP_400_BAD_REQUEST)
+
+        return Response(serializer.data, status=status.HTTP_200_OK)
