c8931400f9657c378554f87ce55d63a929fe0dbd
Bruno-Felix <balvesfelix@gmail.com>
2024-07-01 06:31:56 -0300

    Post user

--- posts/models.py
+++ posts/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from persons.models import Person
+
 class Post(models.Model):
     title = models.CharField(
         max_length = 50,
@@ -21,6 +23,17 @@ class Post(models.Model):
         null = False,
         help_text = ('Link do Post')
     )
+
+    user = models.ForeignKey(
+        Person,
+        blank = False,
+        null = False,
+        on_delete = models.PROTECT,
+    )
+
+    @property
+    def user_name(self):
+        return self.user.name
     
     def __str__(self):
         return self.title
--- posts/serializers.py
+++ posts/serializers.py
@@ -1,7 +1,11 @@
 from rest_framework import serializers
+
 from . import models
+from persons.models import Person
 
 class PostSerializer(serializers.HyperlinkedModelSerializer):
+    user = serializers.PrimaryKeyRelatedField(queryset=Person.objects.all())
+
     class Meta:
         model = models.Post
-        fields = ['url', 'id', 'title', 'description', 'link']
+        fields = ['url', 'id', 'title', 'description', 'user', 'user_name', 'link']
